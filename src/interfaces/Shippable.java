8c2832c8de1645ab50f500acc21797203459f9e5
Omar Ahmed Mahmoud <omargoher59@gmail.com>
2025-07-05 19:33:38 +0300

    Add files via upload

---
+++ src/interfaces/Shippable.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface Shippable {
+    public String getName();
+    public double getWeight();
+}
---
+++ src/model/ShippableProduct.java
@@ -0,0 +1,22 @@
+package model;
+import java.util.*;
+import interfaces.*;
+import model.*;
+import service.*;
+
+public class ShippableProduct extends Product implements Shippable{
+    private double weight;
+
+    public ShippableProduct(String name, double price, int quantity, Date expireDate, double weight) {
+        super(name, price, quantity, expireDate);
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
